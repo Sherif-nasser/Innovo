a28cd3e11c532cf3b56f867d82f6ac8bc1ff84c8
Sherif-nasser <sheriffnasserr@gmail.com>
2023-02-06 17:12:45 +0200

    version one

--- innovo/public/js/appraisal_template.js
+++ innovo/public/js/appraisal_template.js
@@ -1,13 +1,13 @@
-frappe.ui.form.on("Appraisal Template", {
-    onload:function(frm){
-    //   console.log(frm.doc);
-    },
-    create_employee_apprisal:function(frm){
-        frappe.model.open_mapped_doc({
-            method: "innovo.serverside.make_appraisal",
-            frm: frm,
-            args: { salary_component: frm.doc.salary_component },
-            run_link_triggers: true
-        });
-    }
-});
+// frappe.ui.form.on("Appraisal Template", {
+//     onload:function(frm){
+//     //   console.log(frm.doc);
+//     },
+//     create_employee_apprisal:function(frm){
+//         frappe.model.open_mapped_doc({
+//             method: "innovo.serverside.make_appraisal",
+//             frm: frm,
+//             args: { salary_component: frm.doc.salary_component },
+//             run_link_triggers: true
+//         });
+//     }
+// });
